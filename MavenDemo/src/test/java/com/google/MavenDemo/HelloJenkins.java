82d75df4c2deb4483a7ff8bbb6f24924f49509a8
whitecirclecs <deepinder@whitecircleschool.com>
2019-05-28 20:24:40 -0400

    Added a private method

--- MavenDemo/src/test/java/com/google/MavenDemo/HelloJenkins.java
+++ MavenDemo/src/test/java/com/google/MavenDemo/HelloJenkins.java
@@ -2,8 +2,15 @@ package com.google.MavenDemo;
 
 public class HelloJenkins {
 	
+	private void day() {
+		System.out.println("Today is Tuesday");
+	}
+	
 	public static void main(String[] args) {
+		HelloJenkins obj = new HelloJenkins();
+		obj.day();
 		System.out.println("Hello Jenkins!");
+		
 	}
 
 }
--- MavenDemo/src/test/java/com/google/MavenDemo/HelloWorld.java
+++ MavenDemo/src/test/java/com/google/MavenDemo/HelloWorld.java
@@ -3,6 +3,8 @@ package com.google.MavenDemo;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
+		HelloJenkins obj = new HelloJenkins();
+		obj.day();
 		System.out.println("Hello World");
 	}
 
